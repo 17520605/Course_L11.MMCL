190d690fc3c4bcddc487cc5e72af1230fb5768c0
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2020-10-11 17:14:56 +0700

    Nguyễn Hữu Minh Khai Update 11/10/2020
    Android 2020

---
+++ app/src/main/java/Activity/RegisterActivity.java
@@ -0,0 +1,17 @@
+package Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.tutorial_v1.R;
+
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
